ef7c33f9cc5a9512bd87dcb6b6d3cc53db6f90c9
Administrator <sada@sfs.com>
2017-07-12 14:36:51 +0800

    第3次提交 sum()

--- src/test/Ta.java
+++ src/test/Ta.java
@@ -4,6 +4,9 @@ package test;
  * Created by Administrator on 2017/7/12 0012.
  */
 public class Ta {
- private int a=0;
+   private int a=0;
+   public int sum (int x,int y){
+    return x+y;
+   }
 
 }
